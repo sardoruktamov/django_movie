dfd45751a2e2bac936e6cbaed826ab50e2725415
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-08-11 22:24:55 +0500

    admin.py change

--- movies/admin.py
+++ movies/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from .models import Category, Actor, Genre, RatingStar, Rating, Movie, MovieShots, Reviews
 
-# Register your models here.
-
-admin.site.register(Reviews)
+# admin.site.register(Reviews)
 admin.site.register(Actor)
-# admin.site.register(Genre)
-# admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(RatingStar)
 admin.site.register(Rating)
+
+class ReviewInLine(admin.StackedInline):  #ikinchi variant  admin.TabularInline
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email",)
 
 
 @admin.register(Movie)
@@ -19,6 +20,14 @@ class MovieAdmin(admin.ModelAdmin):
     list_filter = ("category", "country", "year",)
     search_fields = ("title", "category__name", "year")
     prepopulated_fields = {"url": ("title",)}
+    inlines = [ReviewInLine]
+    save_on_top = True              #saqlash va o`chirish tugmalarini sahifa yuqorisiga ham qo`shimcha sifatida olib chiqish uchun
+    list_editable = ("draft",)      # admin sahifasida obyectni tashqi sahifadan turib o`zgartirish uchun (bu yerda qoralamani checkbox sifatida ishlatilyapti)
+
+@admin.register(Reviews)
+class RevievAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "parent", "movie", "id",)
+    readonly_fields = ("name", "email",)
 
 
 @admin.register(Genre)
